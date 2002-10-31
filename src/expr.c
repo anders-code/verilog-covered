56f439057a55715ad4ea6386882011c7364da13f
phase1geo <phase1geo@users.sourceforge.net>
2002-10-31 05:22:36 +0000

    Fixing bug with reading in an integer value from the expression line into a short integer.  Needed to use the 'h' value in the sscanf function.  Also added VCSONLYDIAGS variable to regression Makefile for diagnostics that can only run under VCS (not supported by Icarus Verilog).

--- src/expr.c
+++ src/expr.c
@@ -470,7 +470,7 @@ bool expression_db_read( char** line, module* curr_mod, bool eval ) {
   expression  texp;             /* Temporary expression link holder for searching      */
   exp_link*   expl;             /* Pointer to found expression in module               */
 
-  if( sscanf( *line, "%d %s %d %x %d %d%n", &id, modname, &linenum, &suppl, &right_id, &left_id, &chars_read ) == 6 ) {
+  if( sscanf( *line, "%d %s %d %hx %d %d%n", &id, modname, &linenum, &suppl, &right_id, &left_id, &chars_read ) == 6 ) {
 
     *line = *line + chars_read;
 
@@ -1149,6 +1149,12 @@ void expression_dealloc( expression* expr, bool exp_only ) {
 
 /* 
  $Log$
+ Revision 1.58  2002/10/31 05:22:36  phase1geo
+ Fixing bug with reading in an integer value from the expression line into
+ a short integer.  Needed to use the 'h' value in the sscanf function.  Also
+ added VCSONLYDIAGS variable to regression Makefile for diagnostics that can
+ only run under VCS (not supported by Icarus Verilog).
+
  Revision 1.57  2002/10/30 06:07:10  phase1geo
  First attempt to handle expression trees/statement trees that contain
  unsupported code.  These are removed completely and not evaluated (because
