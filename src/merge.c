2fed0861439ef1a5d9613c9cbdda49263e6654e6
phase1geo <phase1geo@users.sourceforge.net>
2002-11-05 00:20:07 +0000

    Adding development documentation.  Fixing problem with combinational logic output in report command and updating full regression.

--- src/merge.c
+++ src/merge.c
@@ -4,8 +4,14 @@
  \date     11/29/2001
 */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <stdio.h>
+#ifdef HAVE_STRING_H
 #include <string.h>
+#endif
 #include <stdlib.h>
 
 #include "db.h"
@@ -151,6 +157,10 @@ int command_merge( int argc, int last_arg, char** argv ) {
 
 /*
  $Log$
+ Revision 1.11  2002/11/05 00:20:07  phase1geo
+ Adding development documentation.  Fixing problem with combinational logic
+ output in report command and updating full regression.
+
  Revision 1.10  2002/11/02 16:16:20  phase1geo
  Cleaned up all compiler warnings in source and header files.
 
