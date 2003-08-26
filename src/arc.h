183e108eac39ff51d7e037e8c437b40132175e77
phase1geo <phase1geo@users.sourceforge.net>
2003-08-26 12:53:35 +0000

    Added initial versions of arc.c and arc.h though they are not even close to being finished at this point.

---
+++ src/arc.h
@@ -0,0 +1,23 @@
+#ifndef __ARC_H__
+#define __ARC_H__
+
+/*!
+ \file     arc.h
+ \author   Trevor Williams  (trevorw@sgi.com)
+ \date     8/25/2003
+ \brief    Contains functions for handling FSM arc arrays.
+*/
+
+#include "defines.h"
+
+
+/*
+ $Log$
+ Revision 1.1  2003/08/26 12:53:35  phase1geo
+ Added initial versions of arc.c and arc.h though they are not even close to
+ being finished at this point.
+
+*/
+
+#endif
+
