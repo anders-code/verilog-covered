38982f1c25f26c82c4ef56811d38b53cff17d1eb
phase1geo <phase1geo@users.sourceforge.net>
2003-08-29 12:52:06 +0000

    Updating comments for functions.

--- src/arc.h
+++ src/arc.h
@@ -8,11 +8,25 @@
  \brief    Contains functions for handling FSM arc arrays.
 */
 
+#include <stdio.h>
+
 #include "defines.h"
 
 
+/*! \brief Writes specified arc array to specified CDD file. */
+bool arc_db_write( char* arcs, FILE* file );
+
+/*! \brief Reads in arc array from CDD database string. */
+bool arc_db_read( char** arcs, char** line );
+
+/*! \brief Deallocates memory for specified arcs array. */
+void arc_dealloc( char* arcs );
+
 /*
  $Log$
+ Revision 1.2  2003/08/29 12:52:06  phase1geo
+ Updating comments for functions.
+
  Revision 1.1  2003/08/26 12:53:35  phase1geo
  Added initial versions of arc.c and arc.h though they are not even close to
  being finished at this point.
