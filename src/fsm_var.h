668b42ab5618e72f89a771e38b2d61dc725dca48
phase1geo <phase1geo@users.sourceforge.net>
2003-11-11 21:48:09 +0000

    Fixing bug where next pointers in bind lists were not being initialized to NULL (manifested itself in Irix).  Also added missing development documentation to functions in fsm_var.c and removed unnecessary function.

--- src/fsm_var.h
+++ src/fsm_var.h
@@ -14,9 +14,6 @@
 /*! \brief Allocates, initializes and adds FSM variable to global list. */
 fsm_var* fsm_var_add( char* mod_name, expression* in_state, expression* out_state, char* name );
 
-/*! \brief Searches FSM variable list for one by the specified name. */
-fsm_var* fsm_var_find_by_name( char* name );
-
 /*! \brief Adds specified signal and expression to binding list. */
 void fsm_var_bind_add( char* sig_name, expression* expr, char* mod_name );
 
@@ -32,6 +29,11 @@ void fsm_var_remove( fsm_var* fv );
 
 /*
  $Log$
+ Revision 1.4  2003/11/11 21:48:09  phase1geo
+ Fixing bug where next pointers in bind lists were not being initialized to
+ NULL (manifested itself in Irix).  Also added missing development documentation
+ to functions in fsm_var.c and removed unnecessary function.
+
  Revision 1.3  2003/10/28 00:18:06  phase1geo
  Adding initial support for inline attributes to specify FSMs.  Still more
  work to go but full regression still passes at this point.
