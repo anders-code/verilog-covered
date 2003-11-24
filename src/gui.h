27d85c35c07357b28a15fd07849faf337e00b870
phase1geo <phase1geo@users.sourceforge.net>
2003-11-24 17:48:56 +0000

    Adding gui.c/.h files for functions related to the GUI interface.  Updated Makefile.am for the inclusion of these files.

---
+++ src/gui.h
@@ -0,0 +1,31 @@
+#ifndef __GUI_H__
+#define __GUI_H__
+
+/*!
+ \file     gui.h
+ \author   Trevor Williams  (trevorw@charter.net)
+ \date     11/24/2003
+ \brief    Contains functions that are used by the GUI report viewer.
+*/
+
+#include "defines.h"
+
+/*! \brief Collects array of uncovered lines from given module. */
+bool line_collect_uncovered( char* mod_name, int** lines, int* line_cnt );
+
+/*! \brief Collects array of module names from the design. */
+bool module_get_list( char*** mod_list, int* mod_size );
+
+/*! \brief Retrieves filename of given module. */
+char* module_get_filename( char* mod_name );
+
+/*
+ $Log$
+ Revision 1.1  2003/11/24 17:48:56  phase1geo
+ Adding gui.c/.h files for functions related to the GUI interface.  Updated
+ Makefile.am for the inclusion of these files.
+
+*/
+
+#endif
+
