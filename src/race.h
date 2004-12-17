dfb5c5d9d56e6229d41a03336d96d09dbc924a77
phase1geo <phase1geo@users.sourceforge.net>
2004-12-17 22:29:36 +0000

    More code added to race condition feature.  Still not usable.

--- src/race.h
+++ src/race.h
@@ -12,6 +12,9 @@
 #include "defines.h"
 
 
+/*! \brief Adds specified input port signal to stmt_sig list */
+void race_add_inport_sig( vsignal* sig );
+
 /*! \brief Parses entire statement tree pointed to by stmt, adding all found signals within it to the stmt_sig list */
 void race_find_and_add_stmt_sigs( statement* stmt, statement* root );
 
@@ -21,6 +24,9 @@ void race_stmt_sig_dealloc( stmt_sig* ss );
 
 /*
  $Log$
+ Revision 1.3  2004/12/17 22:29:36  phase1geo
+ More code added to race condition feature.  Still not usable.
+
  Revision 1.2  2004/12/17 14:27:46  phase1geo
  More code added to race condition checker.  This is in an unusable state at
  this time.
