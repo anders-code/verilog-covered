087027a71d2e47253b03d94b3c2a874cd91edf16
phase1geo <phase1geo@users.sourceforge.net>
2006-03-28 22:28:28 +0000

    Updates to user guide and added copyright information to each source file in the src directory.  Added test directory in user documentation directory containing the example used in line, toggle, combinational logic and FSM descriptions.

--- src/scope.h
+++ src/scope.h
@@ -1,6 +1,21 @@
 #ifndef __SCOPE_H__
 #define __SCOPE_H__
 
+/*
+ Copyright (c) 2006 Trevor Williams
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by the Free Software
+ Foundation; either version 2 of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with this program;
+ if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 /*!
  \file     scope.h
  \author   Trevor Williams  (trevorw@charter.net)
@@ -31,6 +46,11 @@ func_unit* scope_get_parent_module( char* scope );
 
 /*
  $Log$
+ Revision 1.7  2006/03/28 22:28:28  phase1geo
+ Updates to user guide and added copyright information to each source file in the
+ src directory.  Added test directory in user documentation directory containing the
+ example used in line, toggle, combinational logic and FSM descriptions.
+
  Revision 1.6  2006/01/13 23:27:02  phase1geo
  Initial attempt to fix problem with handling functions/tasks/named blocks with
  the same name in the design.  Still have a few diagnostics failing in regressions
