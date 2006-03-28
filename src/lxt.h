860383743f5ac764ff33432dae5ade1f89eb51e7
phase1geo <phase1geo@users.sourceforge.net>
2006-03-28 22:28:27 +0000

    Updates to user guide and added copyright information to each source file in the src directory.  Added test directory in user documentation directory containing the example used in line, toggle, combinational logic and FSM descriptions.

--- src/lxt.h
+++ src/lxt.h
@@ -1,6 +1,21 @@
 #ifndef __LXT_H__
 #define __LXT_H__
 
+/*
+ Copyright (c) 2006 Trevor Williams
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by the Free Software
+ Foundation; either version 2 of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with this program;
+ if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 /*!
  \file     lxt.h
  \author   Trevor Williams  (trevorw@charter.net)
@@ -15,6 +30,11 @@ void lxt_parse( char* lxt_file );
 
 /*
  $Log$
+ Revision 1.2  2006/03/28 22:28:27  phase1geo
+ Updates to user guide and added copyright information to each source file in the
+ src directory.  Added test directory in user documentation directory containing the
+ example used in line, toggle, combinational logic and FSM descriptions.
+
  Revision 1.1  2006/01/26 17:34:04  phase1geo
  Finished work on lxt2_read.c and fixed bug in LXT-based simulator.  Also adding
  missing lxt.h header file.
