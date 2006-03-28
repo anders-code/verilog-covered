4fde38febf51d3188807d053c624c9f0d5545768
phase1geo <phase1geo@users.sourceforge.net>
2006-03-28 22:28:27 +0000

    Updates to user guide and added copyright information to each source file in the src directory.  Added test directory in user documentation directory containing the example used in line, toggle, combinational logic and FSM descriptions.

--- src/binding.h
+++ src/binding.h
@@ -1,6 +1,21 @@
 #ifndef __BINDING_H__
 #define __BINDING_H__
 
+/*
+ Copyright (c) 2006 Trevor Williams
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by the Free Software
+ Foundation; either version 2 of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with this program;
+ if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 /*!
  \file     binding.h
  \author   Trevor Williams  (trevorw@charter.net)
@@ -46,6 +61,11 @@ void bind_dealloc();
 
 /* 
  $Log$
+ Revision 1.26  2006/03/28 22:28:27  phase1geo
+ Updates to user guide and added copyright information to each source file in the
+ src directory.  Added test directory in user documentation directory containing the
+ example used in line, toggle, combinational logic and FSM descriptions.
+
  Revision 1.25  2006/02/16 21:19:26  phase1geo
  Adding support for arrays of instances.  Also fixing some memory problems for
  constant functions and fixed binding problems when hierarchical references are
