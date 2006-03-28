fec0ca41fd2c158a9a9a9997ebf47017adf06812
phase1geo <phase1geo@users.sourceforge.net>
2006-03-28 22:28:27 +0000

    Updates to user guide and added copyright information to each source file in the src directory.  Added test directory in user documentation directory containing the example used in line, toggle, combinational logic and FSM descriptions.

--- src/parse.c
+++ src/parse.c
@@ -1,3 +1,18 @@
+/*
+ Copyright (c) 2006 Trevor Williams
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by the Free Software
+ Foundation; either version 2 of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with this program;
+ if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 /*!
  \file     parse.c
  \author   Trevor Williams  (trevorw@charter.net)
@@ -210,6 +225,11 @@ bool parse_and_score_dumpfile( char* db, char* dump_file, int dump_mode ) {
 
 /*
  $Log$
+ Revision 1.38  2006/03/28 22:28:27  phase1geo
+ Updates to user guide and added copyright information to each source file in the
+ src directory.  Added test directory in user documentation directory containing the
+ example used in line, toggle, combinational logic and FSM descriptions.
+
  Revision 1.37  2006/02/01 19:58:28  phase1geo
  More updates to allow parsing of various parameter formats.  At this point
  I believe full parameter support is functional.  Regression has been updated
