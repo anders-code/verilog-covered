457bc700e76705013ab94afb8c960caa0e8b973c
phase1geo <phase1geo@users.sourceforge.net>
2006-03-28 22:28:27 +0000

    Updates to user guide and added copyright information to each source file in the src directory.  Added test directory in user documentation directory containing the example used in line, toggle, combinational logic and FSM descriptions.

--- src/link.h
+++ src/link.h
@@ -1,6 +1,21 @@
 #ifndef __LINK_H__
 #define __LINK_H__
 
+/*
+ Copyright (c) 2006 Trevor Williams
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by the Free Software
+ Foundation; either version 2 of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with this program;
+ if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 /*!
  \file     link.h
  \author   Trevor Williams  (trevorw@charter.net)
@@ -99,6 +114,11 @@ void funit_link_delete_list( funit_link* head, bool rm_funit );
 
 /*
  $Log$
+ Revision 1.17  2006/03/28 22:28:27  phase1geo
+ Updates to user guide and added copyright information to each source file in the
+ src directory.  Added test directory in user documentation directory containing the
+ example used in line, toggle, combinational logic and FSM descriptions.
+
  Revision 1.16  2005/12/14 23:03:24  phase1geo
  More updates to remove memory faults.  Still a work in progress but full
  regression passes.
