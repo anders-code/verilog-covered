bae92769d2c0143d216ea3a03cdb9491fd5f06f1
phase1geo <phase1geo@users.sourceforge.net>
2006-04-12 13:28:37 +0000

    Fixing problem with memory allocation for merged files.

--- src/info.c
+++ src/info.c
@@ -156,7 +156,7 @@ bool info_db_read( char** line ) {
         *line = *line + chars_read;
 
         /* Add merged file */
-        merge_in = (char**)realloc( merge_in, (sizeof( char* ) * merge_in_num) );
+        merge_in = (char**)realloc( merge_in, (sizeof( char* ) * (merge_in_num + 1)) );
         merge_in[merge_in_num] = strdup_safe( tmp1, __FILE__, __LINE__ );
         merge_in_num++;
 
@@ -194,6 +194,9 @@ bool info_db_read( char** line ) {
 
 /*
  $Log$
+ Revision 1.12  2006/04/12 13:28:37  phase1geo
+ Fixing problem with memory allocation for merged files.
+
  Revision 1.11  2006/04/11 22:42:16  phase1geo
  First pass at adding multi-file merging.  Still need quite a bit of work here yet.
 
