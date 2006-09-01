547c8157ade219eb7b0f3b4a88d93586a0fabe1e
phase1geo <phase1geo@users.sourceforge.net>
2006-09-01 23:06:02 +0000

    Fixing regressions per latest round of changes.  Full regression now passes.

--- src/stat.h
+++ src/stat.h
@@ -32,12 +32,18 @@ statistic* statistic_create();
 /*! \brief Merges the results of the stat_from to the stat_to */
 void statistic_merge( statistic* stat_to, statistic* stat_from );
 
+/*! \brief Returns TRUE if the given statistic structure contains no coverage information */
+bool statistic_is_empty( statistic* stat );
+
 /*! \brief Deallocates memory for a statistic structure. */
 void statistic_dealloc( statistic* stat );
 
 
 /*
  $Log$
+ Revision 1.7  2006/09/01 23:06:02  phase1geo
+ Fixing regressions per latest round of changes.  Full regression now passes.
+
  Revision 1.6  2006/03/28 22:28:28  phase1geo
  Updates to user guide and added copyright information to each source file in the
  src directory.  Added test directory in user documentation directory containing the
