4f2a47d75da9aa2a568bff251e6bda5964832389
phase1geo <phase1geo@users.sourceforge.net>
2006-10-06 22:45:57 +0000

    Added support for the wait() statement.  Added wait1 diagnostic to regression suite to verify its behavior.  Also added missing GPL license note at the top of several *.h and *.c files that are somewhat new.

--- src/enumerate.h
+++ src/enumerate.h
@@ -1,6 +1,21 @@
 #ifndef __ENUMERATE_H__
 #define __ENUMERATE_H__
 
+/*
+ Copyright (c) 2006 Trevor Williams
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by the Free Software
+ Foundation; either version 2 of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with this program;
+ if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 /*!
  \file     enumerate.h
  \author   Trevor Williams  (trevorw@charter.net)
@@ -26,6 +41,11 @@ void enumerate_dealloc_list( func_unit* funit );
 
 /*
  $Log$
+ Revision 1.2  2006/10/06 22:45:57  phase1geo
+ Added support for the wait() statement.  Added wait1 diagnostic to regression
+ suite to verify its behavior.  Also added missing GPL license note at the top
+ of several *.h and *.c files that are somewhat new.
+
  Revision 1.1  2006/08/29 22:49:31  phase1geo
  Added enumeration support and partial support for typedefs.  Added enum1
  diagnostic to verify initial enumeration support.  Full regression has not
