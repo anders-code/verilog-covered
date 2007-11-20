9adfd6a1b296ee39dd6648ba6fb5d4f3fcbdbb8d
phase1geo <phase1geo@users.sourceforge.net>
2007-11-20 05:29:00 +0000

    Updating e-mail address from trevorw@charter.net to phase1geo@gmail.com.

--- src/vcd.h
+++ src/vcd.h
@@ -18,7 +18,7 @@
 
 /*!
  \file     vcd.h
- \author   Trevor Williams (trevorw@charter.net)
+ \author   Trevor Williams (phase1geo@gmail.com)
  \date     7/21/2002
  \brief    Contains VCD parser functions.
 */
@@ -29,6 +29,9 @@ void vcd_parse( char* vcd_file );
 
 /*
  $Log$
+ Revision 1.6  2007/11/20 05:29:00  phase1geo
+ Updating e-mail address from trevorw@charter.net to phase1geo@gmail.com.
+
  Revision 1.5  2006/03/28 22:28:28  phase1geo
  Updates to user guide and added copyright information to each source file in the
  src directory.  Added test directory in user documentation directory containing the
