7dbc72c7371a8cba637bc7d839bbdccaf91d6d66
phase1geo <phase1geo@users.sourceforge.net>
2008-02-01 06:37:07 +0000

    Fixing bug in genprof.pl.  Added initial code for excluding final blocks and using pragma excludes (this code is not fully working yet).  More to be done.

---
+++ diags/verilog/exclude5.3.v
@@ -0,0 +1,32 @@
+module main;
+
+reg  b, d;
+wire c;
+
+wire a = b & c;
+
+assign c = ~b; 
+
+initial begin
+	b = 1'b0;
+	#10;
+	b = 1'b1;
+end
+
+always @(posedge b) d <= ~a;
+
+final begin
+      e = 1'b0;
+      e = 1'b1;
+end
+
+initial begin
+`ifdef DUMP
+        $dumpfile( "exclude5.3.vcd" );
+        $dumpvars( 0, main );
+`endif
+        #100;
+        $finish;
+end
+
+endmodule
