5aeddeb1ae8ae679fc704bf2521a3567df83cb89
phase1geo <phase1geo@users.sourceforge.net>
2008-02-28 07:54:09 +0000

    Starting to add functionality for simulation optimization in the sim_expr_changed function (feature request 1897410).

--- src/sim.h
+++ src/sim.h
@@ -54,6 +54,12 @@ thread* sim_add_thread( thread* parent, statement* stmt, func_unit* funit, const
 /*! \brief Deallocates thread and removes it from parent and thread queue lists for specified functional unit */
 void sim_kill_thread_with_funit( func_unit* funit );
 
+/*! \brief Pushes given thread onto the active queue from the waiting queue */
+void sim_thread_push(
+  thread*         thr,
+  const sim_time* time
+);
+
 /*! \brief Simulates one thread until it has either completed or enters a context switch */
 void sim_thread( thread* thr, const sim_time* time );
 
@@ -75,6 +81,10 @@ void sim_dealloc();
 
 /*
  $Log$
+ Revision 1.35  2008/02/28 07:54:09  phase1geo
+ Starting to add functionality for simulation optimization in the sim_expr_changed
+ function (feature request 1897410).
+
  Revision 1.34  2008/02/27 05:26:51  phase1geo
  Adding support for $finish and $stop.
 
--- src/statement.c
+++ src/statement.c
@@ -325,7 +325,7 @@ void statement_db_write( statement* stmt, FILE* ofile, bool parse_mode ) { PROFI
   fprintf( ofile, "%d %d %x %d %d",
     DB_TYPE_STATEMENT,
     expression_get_id( stmt->exp, parse_mode ),
-    (stmt->suppl.all & 0x1ff),
+    (stmt->suppl.all & 0xff),
     ((stmt->next_true   == NULL) ? 0 : expression_get_id( stmt->next_true->exp, parse_mode )),
     ((stmt->next_false  == NULL) ? 0 : expression_get_id( stmt->next_false->exp, parse_mode ))
   );
@@ -956,6 +956,10 @@ void statement_dealloc( statement* stmt ) { PROFILE(STATEMENT_DEALLOC);
 
 /*
  $Log$
+ Revision 1.126  2008/02/28 07:54:09  phase1geo
+ Starting to add functionality for simulation optimization in the sim_expr_changed
+ function (feature request 1897410).
+
  Revision 1.125  2008/02/25 20:43:49  phase1geo
  Checking in code to allow the use of racecheck pragmas.  Added new tests to
  regression suite to verify this functionality.  Still need to document in
