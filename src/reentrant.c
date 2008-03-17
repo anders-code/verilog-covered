dad183c20522de543fcbf4723017fb5ec8547ea1
phase1geo <phase1geo@users.sourceforge.net>
2008-03-17 05:26:17 +0000

    Checkpointing.  Things don't compile at the moment.

--- src/reentrant.c
+++ src/reentrant.c
@@ -269,12 +269,12 @@ void reentrant_dealloc( reentrant* ren, func_unit* funit, expression* expr ) { P
       reentrant_restore_data_bits( funit, ren, 0, expr );
 
       /* Deallocate the data nibble array */
-      free_safe( ren->data );
+      free_safe( ren->data, (sizeof( nibble ) * ren->data_size) );
 
     }
 
     /* Deallocate memory allocated for this reentrant structure */
-    free_safe( ren );
+    free_safe( ren, sizeof( reentrant ) );
 
   }
 
@@ -282,6 +282,9 @@ void reentrant_dealloc( reentrant* ren, func_unit* funit, expression* expr ) { P
 
 /*
  $Log$
+ Revision 1.17  2008/03/17 05:26:17  phase1geo
+ Checkpointing.  Things don't compile at the moment.
+
  Revision 1.16  2008/01/16 23:10:33  phase1geo
  More splint updates.  Code is now warning/error free with current version
  of run_splint.  Still have regression issues to debug.
