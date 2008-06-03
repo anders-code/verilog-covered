7f23c78e2efe68c58be981f5f7fce0aa2c43d2ca
phase1geo <phase1geo@users.sourceforge.net>
2008-06-03 13:03:08 +0000

    More wizard window updates for GUI.

--- scripts/wizard.tcl
+++ scripts/wizard.tcl
@@ -1,5 +1,7 @@
 proc create_wizard {} {
 
+  global show_wizard
+
   # Now create the window and set the grab to this window
   if {[winfo exists .wizwin] == 0} {
 
@@ -11,13 +13,20 @@ proc create_wizard {} {
     frame .wizwin.f -relief raised -borderwidth 1 
 
     # Create buttons
-    button .wizwin.f.new   -text "Generate New CDD File" -command {
+    button .wizwin.f.new   -text "Generate New CDD File" -relief flat -state disabled -command {
     }
-    button .wizwin.f.open  -text "Open/Merge CDD File(s)" -command {
+    button .wizwin.f.open  -text "Open/Merge CDD File(s)" -relief flat -command {
+      .menubar.file invoke 0
+      destroy .wizwin
     }
-    button .wizwin.f.grade -text "Grade CDD Files" -command {
+    button .wizwin.f.grade -text "Grade CDD Files" -relief flat -state disabled -command {
     }
-    button .wizwin.f.help  -text "Get Help" -command {
+    button .wizwin.f.help  -text "Get Help" -relief flat -state disabled -command {
+    }
+
+    # Checkbutton to disable this window
+    checkbutton .wizwin.f.show -text "Display this window at startup of Covered" -variable show_wizard -onvalue true -offvalue false -command {
+      write_coveredrc
     }
 
     # Pack the buttons
@@ -25,12 +34,14 @@ proc create_wizard {} {
     grid .wizwin.f.open  -row 1 -column 0 -sticky news -padx 4 -pady 4
     grid .wizwin.f.grade -row 2 -column 0 -sticky news -padx 4 -pady 4
     grid .wizwin.f.help  -row 3 -column 0 -sticky news -padx 4 -pady 4
+    grid .wizwin.f.show  -row 4 -column 0 -sticky news -padx 4 -pady 4
 
     # Pack the frame
     pack .wizwin.f
 
   }
 
+  # Make sure that the focus is on this window only
   grab .wizwin
 
 }
