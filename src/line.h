45f147d86f7bc6a0745e3b8bf256da35087cf524
phase1geo <phase1geo@users.sourceforge.net>
2008-07-10 22:43:52 +0000

    Merging in rank-devel-branch into this branch.  Added -f options for all commands to allow files containing command-line arguments to be added.  A few error diagnostics are currently failing due to changes in the rank branch that never got fixed in that branch.  Checkpointing.

---
+++ src/line.h
@@ -0,0 +1,142 @@
+#ifndef __LINE_H__
+#define __LINE_H__
+
+/*
+ Copyright (c) 2006 Trevor Williams
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by the Free Software
+ Foundation; either version 2 of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with this program;
+ if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+/*!
+ \file     line.h
+ \author   Trevor Williams  (phase1geo@gmail.com)
+ \date     3/31/2002
+ \brief    Contains functions for determining/reporting line coverage.
+*/
+
+#include <stdio.h>
+
+#include "defines.h"
+
+/*! \brief Calculates line coverage numbers for the specified expression list. */
+void line_get_stats(
+            func_unit*    funit,
+  /*@out@*/ unsigned int* total,
+  /*@out@*/ unsigned int* hit
+);
+
+/*! \brief Gathers line numbers from specified functional unit that were not hit during simulation. */
+bool line_collect(
+            const char* funit_name,
+            int         funit_type,
+            int         cov,
+  /*@out@*/ int**       lines,
+  /*@out@*/ int**       excludes,
+  /*@out@*/ int*        line_cnt,
+  /*@out@*/ int*        line_size
+);
+
+/*! \brief Generates report output for line coverage. */
+void line_report(
+  FILE* ofile,
+  bool  verbose
+);
+
+
+/*
+ $Log$
+ Revision 1.18.6.1  2008/07/10 22:43:52  phase1geo
+ Merging in rank-devel-branch into this branch.  Added -f options for all commands
+ to allow files containing command-line arguments to be added.  A few error diagnostics
+ are currently failing due to changes in the rank branch that never got fixed in that
+ branch.  Checkpointing.
+
+ Revision 1.20  2008/06/27 14:02:02  phase1geo
+ Fixing splint and -Wextra warnings.  Also fixing comment formatting.
+
+ Revision 1.19  2008/06/22 05:08:40  phase1geo
+ Fixing memory testing error in tcl_funcs.c.  Removed unnecessary output in main_view.tcl.
+ Added a few more report diagnostics and started to remove width report files (these will
+ no longer be needed and will improve regression runtime and diagnostic memory footprint.
+
+ Revision 1.18  2008/01/16 05:01:22  phase1geo
+ Switched totals over from float types to int types for splint purposes.
+
+ Revision 1.17  2008/01/07 23:59:55  phase1geo
+ More splint updates.
+
+ Revision 1.16  2007/11/20 05:28:58  phase1geo
+ Updating e-mail address from trevorw@charter.net to phase1geo@gmail.com.
+
+ Revision 1.15  2007/04/02 04:50:04  phase1geo
+ Adding func_iter files to iterate through a functional unit for reporting
+ purposes.  Updated affected files.
+
+ Revision 1.14  2006/06/26 04:12:55  phase1geo
+ More updates for supporting coverage exclusion.  Still a bit more to go
+ before this is working properly.
+
+ Revision 1.13  2006/03/28 22:28:27  phase1geo
+ Updates to user guide and added copyright information to each source file in the
+ src directory.  Added test directory in user documentation directory containing the
+ example used in line, toggle, combinational logic and FSM descriptions.
+
+ Revision 1.12  2005/11/10 19:28:23  phase1geo
+ Updates/fixes for tasks/functions.  Also updated Tcl/Tk scripts for these changes.
+ Fixed bug with net_decl_assign statements -- the line, start column and end column
+ information was incorrect, causing problems with the GUI output.
+
+ Revision 1.11  2005/11/08 23:12:09  phase1geo
+ Fixes for function/task additions.  Still a lot of testing on these structures;
+ however, regressions now pass again so we are checkpointing here.
+
+ Revision 1.10  2004/03/16 05:45:43  phase1geo
+ Checkin contains a plethora of changes, bug fixes, enhancements...
+ Some of which include:  new diagnostics to verify bug fixes found in field,
+ test generator script for creating new diagnostics, enhancing error reporting
+ output to include filename and line number of failing code (useful for error
+ regression testing), support for error regression testing, bug fixes for
+ segmentation fault errors found in field, additional data integrity features,
+ and code support for GUI tool (this submission does not include TCL files).
+
+ Revision 1.9  2003/11/22 20:44:58  phase1geo
+ Adding function to get array of missed line numbers for GUI purposes.  Updates
+ to report command for getting information ready when running the GUI.
+
+ Revision 1.8  2002/11/05 00:20:07  phase1geo
+ Adding development documentation.  Fixing problem with combinational logic
+ output in report command and updating full regression.
+
+ Revision 1.7  2002/10/31 23:13:55  phase1geo
+ Fixing C compatibility problems with cc and gcc.  Found a few possible problems
+ with 64-bit vs. 32-bit compilation of the tool.  Fixed bug in parser that
+ lead to bus errors.  Ran full regression in 64-bit mode without error.
+
+ Revision 1.6  2002/10/29 19:57:50  phase1geo
+ Fixing problems with beginning block comments within comments which are
+ produced automatically by CVS.  Should fix warning messages from compiler.
+
+ Revision 1.5  2002/09/13 05:12:25  phase1geo
+ Adding final touches to -d option to report.  Adding documentation and
+ updating development documentation to stay in sync.
+
+ Revision 1.4  2002/06/25 21:46:10  phase1geo
+ Fixes to simulator and reporting.  Still some bugs here.
+
+ Revision 1.3  2002/05/13 03:02:58  phase1geo
+ Adding lines back to expressions and removing them from statements (since the line
+ number range of an expression can be calculated by looking at the expression line
+ numbers).
+*/
+
+#endif
+
