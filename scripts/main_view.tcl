10767dd4ba332a0ce06c22ac255d1e19f4a07910
phase1geo <phase1geo@users.sourceforge.net>
2008-08-14 06:46:19 +0000

    Initial attempt to add support for tooltips.  Checkpointing.

--- scripts/main_view.tcl
+++ scripts/main_view.tcl
@@ -677,6 +677,62 @@ proc help_button {w file {section ""}} {
 
 }
 
+#################################################################
+# Implementation of a tooltip system taken from the Tcl/Tk Wiki #
+#################################################################
+
+# Main tooltip function to call for creating the tooltip
+proc setTooltip {widget text} {
+
+  if { $text != "" } {
+
+    # 2) Adjusted timings and added key and button bindings. These seem to
+    # make artifacts tolerably rare.
+    bind $widget <Any-Enter>    [list after 1000 [list showTooltip %W $text]]
+    bind $widget <Any-Leave>    [list destroy %W.tooltip]
+    bind $widget <Any-KeyPress> [list destroy %W.tooltip]
+    bind $widget <Any-Button>   [list destroy %W.tooltip]
+
+  }
+
+}
+
+proc showTooltip {widget text} {
+
+  global tcl_platform
+
+  if { [string match $widget* [winfo containing  [winfo pointerx .] [winfo pointery .]] ] == 0  } {
+    return
+  }
+
+  catch { destroy $widget.tooltip }
+
+  set scrh [winfo screenheight $widget]    ; # 1) flashing window fix
+  set scrw [winfo screenwidth $widget]     ; # 1) flashing window fix
+  set tooltip [toplevel $widget.tooltip -bd 1 -bg black]
+  wm geometry $tooltip +$scrh+$scrw        ; # 1) flashing window fix
+  wm overrideredirect $tooltip 1
+
+#  if {$tcl_platform(platform) == {windows}} { ; # 3) wm attributes...
+    wm attributes $tooltip -topmost 1         ; # 3) assumes...
+#  }                                           ; # 3) Windows
+  pack [label $tooltip.label -bg lightyellow -fg black -text $text -justify left]
+
+  set width [winfo reqwidth $tooltip.label]
+  set height [winfo reqheight $tooltip.label]
+
+  set positionX [winfo pointerx .]
+  set positionY [expr [winfo pointery .] + 25]
+
+  wm geometry $tooltip [join  "$width x $height + $positionX + $positionY" {}]
+  raise $tooltip
+
+  # 2) Kludge: defeat rare artifact by passing mouse over a tooltip to destroy it.
+  bind $widget.tooltip <Any-Enter> {destroy %W}
+  bind $widget.tooltip <Any-Leave> {destroy %W}
+
+}
+
 # Read configuration file
 read_coveredrc
 
