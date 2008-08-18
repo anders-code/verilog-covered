d2d9e14b92eb83998a24018d4e7ad8a15b1ed284
phase1geo <phase1geo@users.sourceforge.net>
2008-08-18 23:07:26 +0000

    Integrating changes from development release branch to main development trunk. Regression passes.  Still need to update documentation directories and verify that the GUI stuff works properly.

--- src/expr.h
+++ src/expr.h
@@ -141,8 +141,10 @@ void expression_dealloc( expression* expr, bool exp_only );
 
 /*
  $Log$
- Revision 1.70  2008/07/29 06:34:21  phase1geo
- Merging in contents of development branch to the main development trunk.
+ Revision 1.71  2008/08/18 23:07:26  phase1geo
+ Integrating changes from development release branch to main development trunk.
+ Regression passes.  Still need to update documentation directories and verify
+ that the GUI stuff works properly.
 
  Revision 1.68.2.1  2008/07/10 22:43:50  phase1geo
  Merging in rank-devel-branch into this branch.  Added -f options for all commands
