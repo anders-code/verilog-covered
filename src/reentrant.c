3ced8d65c433ac18787d9a10d696981ca38f7373
phase1geo <phase1geo@users.sourceforge.net>
2008-08-18 23:07:28 +0000

    Integrating changes from development release branch to main development trunk. Regression passes.  Still need to update documentation directories and verify that the GUI stuff works properly.

--- src/reentrant.c
+++ src/reentrant.c
@@ -338,8 +338,10 @@ void reentrant_dealloc(
 
 /*
  $Log$
- Revision 1.21  2008/07/29 06:34:22  phase1geo
- Merging in contents of development branch to the main development trunk.
+ Revision 1.22  2008/08/18 23:07:28  phase1geo
+ Integrating changes from development release branch to main development trunk.
+ Regression passes.  Still need to update documentation directories and verify
+ that the GUI stuff works properly.
 
  Revision 1.18.2.1  2008/07/10 22:43:54  phase1geo
  Merging in rank-devel-branch into this branch.  Added -f options for all commands
