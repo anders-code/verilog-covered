ea400b4a8d6919ddeb56a30700ba979ae8980e8b
phase1geo <phase1geo@users.sourceforge.net>
2008-10-21 05:38:42 +0000

    More updates to support real values.  Added vector_from_real64 functionality. Checkpointing.

--- src/static.c
+++ src/static.c
@@ -155,7 +155,7 @@ static_expr* static_expr_gen_unary(
             vector_dealloc( tmpexp->value );
             tmpexp->value = vec;
           }
-          (void)vector_from_int( tmpexp->value, stexp->num, FALSE );
+          (void)vector_from_int( tmpexp->value, stexp->num );
 
           stexp->exp = expression_create( tmpexp, NULL, op, FALSE, curr_expr_id, line, first, last, FALSE );
           curr_expr_id++;
@@ -260,7 +260,7 @@ static_expr* static_expr_gen(
           vector_dealloc( right->exp->value );
           right->exp->value = vec;
         }
-        (void)vector_from_int( right->exp->value, right->num, FALSE );
+        (void)vector_from_int( right->exp->value, right->num );
 
         tmpexp = expression_create( right->exp, left->exp, op, FALSE, curr_expr_id, line, first, last, FALSE );
         curr_expr_id++;
@@ -279,7 +279,7 @@ static_expr* static_expr_gen(
           vector_dealloc( left->exp->value );
           left->exp->value = vec;
         }
-        (void)vector_from_int( left->exp->value, left->num, FALSE );
+        (void)vector_from_int( left->exp->value, left->num );
 
         tmpexp = expression_create( right->exp, left->exp, op, FALSE, curr_expr_id, line, first, last, FALSE );
         curr_expr_id++;
@@ -454,6 +454,10 @@ void static_expr_dealloc(
 
 /*
  $Log$
+ Revision 1.41  2008/10/21 05:38:42  phase1geo
+ More updates to support real values.  Added vector_from_real64 functionality.
+ Checkpointing.
+
  Revision 1.40  2008/10/20 23:20:02  phase1geo
  Adding support for vector_from_int coverage accumulation (untested at this point).
  Updating Cver regressions.  Checkpointing.
